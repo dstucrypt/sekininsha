825f8cd28eb75249ef54379c061092928ed0f616
Ilya Petrov <ilya.muromec@gmail.com>
2014-09-26 20:44:34 +0300

    Add stub API to resolve users:
    curl 'http://vote.local/api/1/user/200'
    curl 'http://vote.local/api/1/user/201'
    curl 'http://vote.local/api/1/user/email:mustafa@h.tv'
    curl 'http://vote.local/api/1/user/tax_id:2952222000'
    {
      "status": "ok",
      "user": {
        "have_eusign": true,
        "have_facebook": true,
        "name": "Mozes CashKing",
        "user_id": 201
      }
    }
    
    curl 'http://vote.local/api/1/user/tax_id:2952222001'
    {
      "message": "Not found",
      "status": "fail"
    }
    
    Note, that tax_id is fixed-length field and should contain exactly
    ten digits.

--- sekininsha/api.py
+++ sekininsha/api.py
@@ -25,3 +25,35 @@ def api_group_create():
         return jsonify(status='error', message='Member format'), 400
 
     return jsonify(status='ok', group_id=1)
+
+
+@app.route('/api/1/user/<lookup>')
+def api_user_resolve(lookup):
+    uid = None
+    if lookup.startswith('tax_id:') or \
+       lookup.startswith('email:'):
+        kw = dict((lookup.split(':', 1), ))
+    else:
+        try:
+            uid = int(lookup)
+            kw = {"id": uid}
+        except:
+            return jsonify(status='error', message="Don't understand"), 404
+
+    if kw.get('email') == 'mustafa@h.tv' or uid == 200:
+        user = {
+            "user_id": 200,
+            "name": "Mustafa Nayem",
+            "have_facebook": True,
+        }
+    elif kw.get('tax_id') == '2952222000' or uid == 201:
+        user = {
+            "user_id": 201,
+            "name": "Mozes CashKing",
+            "have_facebook": True,
+            "have_eusign": True,
+        }
+    else:
+        return jsonify(status='fail', message='Not found'), 404
+
+    return jsonify(status='ok', user=user)
