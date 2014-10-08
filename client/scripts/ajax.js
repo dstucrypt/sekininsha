7037d9dabde9fdf643b80e7c81404397bd21e237
Ilya Petrov <ilya.muromec@gmail.com>
2014-10-08 17:27:12 +0300

    fgtn ajax script

---
+++ client/scripts/ajax.js
@@ -0,0 +1,21 @@
+var resp_done = function(cb, req) {
+    var resp;
+    try {
+        resp = JSON.parse(req.responseText);
+    } catch (e) {
+    }
+
+    cb(req, resp);
+};
+module.exports = function (url, cb, data) {
+    var req = new XMLHttpRequest();
+    req.open(data ? 'POST' : 'GET', url, true);
+    req.overrideMimeType('application/json');
+    if(data) {
+        req.setRequestHeader('Content-Type', 'application/json');
+    }
+    req.onerror = cb.bind(req);
+    req.ontimeout = cb.bind(req);
+    req.onload = resp_done.bind(null, cb, req);
+    req.send(JSON.stringify(data));
+};
