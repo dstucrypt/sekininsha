3ea8c64f98a5994917668c3082e4c48f2dc4c475
Nikita Dudnik <nikdudnik@gmail.com>
2014-10-13 00:31:39 +0300

    Add NavItemLink component.
    Basically overwrite NavItem's onSelect event.
    It's also known as onClick in other components but has this name
    because NavItems never go alone and clicking it is selecting it
    from a list.
    The Navigation mix in provides this.transitionTo used to go
    to the route.
    The ActiveState mix in is used to check that current route
    is the same as the NavItem's route and so the NavItem
    is active. It doesn't work though.

---
+++ client/scripts/NavItemLink.js
@@ -0,0 +1,50 @@
+/** @jsx React.DOM */
+'use strict';
+var ActiveState = require('react-router').ActiveState;
+var Navigation = require('react-router').Navigation;
+var NavItem = require('react-bootstrap/NavItem');
+var merge = require('react/lib/merge');
+
+var React = require('react');
+
+var NavItemLink = React.createClass({
+  displayName: 'NavItemLink',
+  mixins: [ActiveState, Navigation],
+  propTypes: {
+  	activeClassName: React.PropTypes.string.isRequired,
+    to: React.PropTypes.string.isRequired,
+    params: React.PropTypes.object,
+    query: React.PropTypes.object,
+    onSelect: React.PropTypes.func
+  },
+  getDefaultProps: function () {
+    return {
+      activeClassName: 'active'
+    };
+  },
+  handleSelect: function (event) {
+      this.transitionTo(this.props.to, this.props.params, this.props.query);
+  },
+  getClassName: function () {
+    var className = this.props.className || '';
+
+    if (this.isActive(this.props.to, this.props.params, this.props.query))
+      className += ' ' + this.props.activeClassName;
+
+    return className;
+  },
+  getHref: function () {
+    return this.makeHref(this.props.to, this.props.params, this.props.query);
+  },
+  render: function() {
+  	var isActive = this.isActive(this.props.to, this.props.params, this.props.query);
+  	var props = merge(this.props, {
+    	href: this.getHref(),
+      	className: this.getClassName(),
+      	onSelect: this.handleSelect
+    });
+    return NavItem(props, this.props.children);
+  }
+});
+
+module.exports = NavItemLink;
