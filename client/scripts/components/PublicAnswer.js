08d33c2ac5bf526d26cf2e638a13fef02c5cd0d7
Nikita Dudnik <nikdudnik@gmail.com>
2014-10-13 18:03:04 +0300

    Add vote state visualization.
    Use icons and add tooltip to the most cryptic state.

--- client/scripts/components/PublicAnswer.js
+++ client/scripts/components/PublicAnswer.js
@@ -1,6 +1,8 @@
 /** @jsx React.DOM */
 'use strict';
 var React = require('react/addons');
+var OverlayTrigger = require('react-bootstrap').OverlayTrigger;
+var Tooltip = require('react-bootstrap').Tooltip;
 
 var PublicAnswer = React.createClass({
     render: function() {
@@ -12,19 +14,19 @@ var PublicAnswer = React.createClass({
 
         switch (answer) {
             case "yes":
-                answer = <span className="glyphicon glyphicon-ok"/>;
+                answer = <span className="glyphicon glyphicon-ok text-success"/>;
                 break;
             case "no": 
-                answer = "НИЕТ";
+                answer = <span className="glyphicon glyphicon-remove text-danger"/>;
                 break;
             case "skip":
-                answer = "проп";
+                answer = <span className="glyphicon glyphicon-minus"/>;
                 break;
             case "?":
-                answer = "???";
+                answer = <span className="glyphicon glyphicon-time text-info"/>;
                 break;
             case "X":
-                answer = "XXX";
+                answer = <OverlayTrigger placement="right" overlay={<Tooltip>Пользователь приглашен, но не зарегистрировался.</Tooltip>}><span className="glyphicon glyphicon-warning-sign text-info"/></OverlayTrigger>;
                 break;
             default:
                 break;
